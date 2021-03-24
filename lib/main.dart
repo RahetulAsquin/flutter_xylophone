392bc8e43cc8a681e157b8a880b64ff27de22b73
RahetulAsquin <rahetulasquin12345@gmail.com>
2021-03-24 14:07:53 +0530

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:audioplayers/audioplayers.dart';
+
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
@@ -9,7 +8,14 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Center(
+            child: Text('Xylophone'),
+          ),
+        ),
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
